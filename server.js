2d35b9ce25e78018432ee17467a4f87dd2d4cbaa
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-29 09:43:33 +0200

    added app.use(express.static("public"))

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import express from "express";
 const app = express();
 const port = 3068;
 
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
   res.send(`<h1>Flaschcard API</h1>`);
 });
