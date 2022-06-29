8294a096216e88170e1ff2d9a6f325290afce30c
Bekir - CDI <b.burakcebecii@gmail.com>
2022-06-29 11:18:02 +0200

    added post for flashcards..

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require("express");
-const { sequelize } = require("./models");
+const { sequelize, Flaschcard } = require("./models");
 
 const app = express();
 const port = 3068;
+
+app.use(express.json());
 
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.send(`<h1>Flaschcard API</h1>`);
+});
+
+app.post("/flashcards", async (req, res) => {
+  const { category, front, back } = req.body;
+  try {
+    const flashcard = await Flashcard.create({ category, front, back });
+    return res.json(flashcard);
+  } catch (err) {
+    console.log(err);
+    return res.status(500);
+  }
 });
 
 app.listen(port, async () => {
